cf7acc83536cf3de542c85ef4f9a3a63b5570331
isaquecostaaa <167151390+isaquecostaaa@users.noreply.github.com>
2024-05-01 20:01:57 -0300

    adicionada definicao de adicional e semantica

--- firstapp/script.js
+++ firstapp/script.js
@@ -2,30 +2,31 @@
 
 const SALARIO_ATE_20 = 1000
 const SALARIO_ACIMA_20 = 2000
+const IDADE_LIMITE = 20
 
 //input
 
-anoNascimento = parseInt(prompt("Informe o seu ano de nascimento"))
-nome = prompt("Informe o seu nome")
-salarioBase = parseFloat(prompt("Informe seu salário base"))
-gratificacao = parseFloat(prompt("Informe o valor da sua gratificação"))
-bonus = parseFloat(prompt("Informe o valor do seu bônus"))
-desconto = parseFloat(prompt("Informe o valor do seu desconto"))
-salarioLiquido = 0
-adicional = 0
+const ANO_NASCIMENTO = parseInt(prompt("Informe o seu ano de nascimento"))
+const NOME = prompt("Informe o seu nome")
+const SALARIO_BASE = parseFloat(prompt("Informe seu salário base"))
+const GRATIFICACAO = parseFloat(prompt("Informe o valor da sua gratificação"))
+const BONUS = parseFloat(prompt("Informe o valor do seu bônus"))
+const DESCONTO = parseFloat(prompt("Informe o valor do seu desconto"))
+
 
 //Processamento
 
-hoje = new Date()
-anoAtual = hoje.getFullYear()
-idade = anoAtual - anoNascimento
-if (idade <= 20 ) {
-adicional = SALARIO_ATE_20 
-} else {
-adicional = SALARIO_ACIMA_20 }
-salarioLiquido = salarioBase + gratificacao + bonus - desconto + adicional
+const HOJE = new Date()
+const ANO_ATUAL = HOJE.getFullYear()
+const IDADE = ANO_ATUAL - ANO_NASCIMENTO
+
+let adicional = SALARIO_ACIMA_20
+if (IDADE <= IDADE_LIMITE) {
+adicional = SALARIO_ATE_20 }
+
+let salarioLiquido = SALARIO_BASE + GRATIFICACAO + BONUS - DESCONTO + adicional
 
 
 //output
-mensagem = ("Eu sou " + nome + ", tenho " + idade + " anos e recebo R$" + salarioLiquido)
+let mensagem = ("Eu sou " + NOME + ", tenho " + IDADE + " anos e recebo R$" + salarioLiquido)
 alert(mensagem)
